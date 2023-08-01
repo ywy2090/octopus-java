7b5f576467748e1508d36f016ec83811058ded65
octopus <912554887@qq.com>
2023-08-01 12:08:03 +0800

    init

---
+++ src/jmh/java/jmh/benchmarks/Main.java
@@ -0,0 +1,27 @@
+package jmh.benchmarks;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Options opt =
+        new OptionsBuilder()
+            .include("jmh.benchmarks.*")
+            .forks(1)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .warmupIterations(6)
+            .warmupTime(TimeValue.valueOf("1s"))
+            .measurementIterations(10)
+            .measurementTime(TimeValue.valueOf("1s"))
+            // .resultFormat(ResultFormatType.JSON)
+            .resultFormat(ResultFormatType.TEXT)
+            .build();
+
+    new Runner(opt).run();
+  }
+}
